c81c16623c792c24660a588286f8b45df15be937
Alan Bruno <alan.brunoab@gmail.com>
2023-04-17 13:46:42 -0300

    Update container props

--- app/components/Container.tsx
+++ app/components/Container.tsx
@@ -3,7 +3,7 @@
 interface ContainerProps {
   children: React.ReactNode;
 }
-const Container: React.FC<ContainerProps> = ({ children }) => {
+const Container = ({ children }: ContainerProps) => {
   return <div className="mx-auto max-w-[2520px] px-4 xl:px-20">{children}</div>;
 };
 
