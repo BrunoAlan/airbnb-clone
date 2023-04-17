e6c5ed8ee54cd14230840ef6c71addb31c168c43
Alan Bruno <alan.brunoab@gmail.com>
2023-04-17 13:46:27 -0300

    Added search icon

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,4 +1,7 @@
 "use client";
+
+import { BiSearch } from "react-icons/bi";
+
 const Search = () => {
   return (
     <div className="w-full cursor-pointer rounded-full border-[1px] py-2 shadow-sm transition hover:shadow-md md:w-auto">
@@ -9,6 +12,9 @@ const Search = () => {
         </div>
         <div className="flex flex-row items-center gap-3 pl-6 pr-2 text-center text-sm text-gray-600">
           <div className="hidden sm:block">Add Guests</div>
+          <div className="rounded-full bg-rose-500 p-2 text-white">
+            <BiSearch size={18} />
+          </div>
         </div>
       </div>
     </div>
