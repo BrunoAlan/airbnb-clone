873c9daf44a20ead0d435175b8d2987aa7ccfa8e
Alan Bruno <alan.brunoab@gmail.com>
2023-04-18 13:49:49 -0300

    Add contentBody to RentModal

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -2,16 +2,78 @@
 import useRentModal from "@/app/hooks/useRentModal";
 
 import Modal from "./Modal";
+import { useMemo, useState } from "react";
+import Heading from "../Heading";
+import { categories } from "../navbar/Categories";
+import CategoryInput from "../inputs/CategoryInput";
+
+enum STEPS {
+  CATEGORY = 0,
+  LOCATION = 1,
+  INFO = 2,
+  IMAGES = 3,
+  DESCRIPTION = 4,
+  PRICE = 5,
+}
 
 const RentModal = () => {
   const rentModal = useRentModal();
+
+  const [step, setSetps] = useState(STEPS.CATEGORY);
+
+  const onBack = () => {
+    setSetps((value) => value - 1);
+  };
+
+  const onNext = () => {
+    setSetps((value) => value + 1);
+  };
+
+  const actionLabel = useMemo(() => {
+    if (step === STEPS.PRICE) {
+      return "Create";
+    }
+    return "Next";
+  }, [step]);
+
+  const secondaryActionLabel = useMemo(() => {
+    if (step === STEPS.CATEGORY) {
+      return undefined;
+    }
+    return "Back";
+  }, [step]);
+
+  let bodyContent = (
+    <div className="flex flex-col gap-8">
+      <Heading
+        title="Which of these best describe your place?"
+        subtitle="Pick a category"
+      />
+      <div className="grid max-h-[50vh] grid-cols-1 gap-3 overflow-auto md:grid-cols-2">
+        {categories.map((item) => (
+          <div key={item.label} className="col-span-1">
+            <CategoryInput
+              onClick={() => {}}
+              selected={false}
+              label={item.label}
+              icon={item.icon}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+
   return (
     <Modal
       title="Airbnb your home!"
       onClose={rentModal.onClose}
       onSubmit={rentModal.onClose}
       isOpen={rentModal.isOpen}
-      actionLabel="Submit"
+      actionLabel={actionLabel}
+      secondaryActionLabel={secondaryActionLabel}
+      secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
+      body={bodyContent}
     />
   );
 };
