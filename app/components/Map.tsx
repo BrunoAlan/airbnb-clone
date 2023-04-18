7b95a97ecf2151b1c40bd2e422e903a70db3b8f5
Alan Bruno <alan.brunoab@gmail.com>
2023-04-18 15:27:53 -0300

    Fixed wrong mergeOption on Map component

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -1,6 +1,8 @@
 "use client";
+
 import L from "leaflet";
 import { MapContainer, Marker, TileLayer } from "react-leaflet";
+
 import "leaflet/dist/leaflet.css";
 import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
@@ -10,16 +12,19 @@ import markerShadow from "leaflet/dist/images/marker-shadow.png";
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconUrl: markerIcon.src,
-  iconRetinaUrl: markerIcon2x,
-  shadowUrl: markerShadow,
+  iconRetinaUrl: markerIcon2x.src,
+  shadowUrl: markerShadow.src,
 });
 
 interface MapProps {
   center?: number[];
 }
 
-const Map = (props: MapProps) => {
-  const { center } = props;
+const url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+const attribution =
+  '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+
+const Map: React.FC<MapProps> = ({ center }) => {
   return (
     <MapContainer
       center={(center as L.LatLngExpression) || [51, -0.09]}
@@ -27,10 +32,8 @@ const Map = (props: MapProps) => {
       scrollWheelZoom={false}
       className="h-[35vh] rounded-lg"
     >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />{" "}
+      <TileLayer url={url} attribution={attribution} />
+      {center && <Marker position={center as L.LatLngExpression} />}
     </MapContainer>
   );
 };
