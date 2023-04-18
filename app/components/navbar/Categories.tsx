187c8f98eb4194cd9445e99b3b7cf2e9f389b8ad
Alan Bruno <alan.brunoab@gmail.com>
2023-04-18 11:00:56 -0300

    Handle selected category

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -3,6 +3,7 @@ import { TbBeach } from "react-icons/tb";
 import { GiWindmill } from "react-icons/gi";
 import { MdOutlineVilla } from "react-icons/md";
 import CategoryBox from "../CategoryBox";
+import { usePathname, useSearchParams } from "next/navigation";
 
 export const categories = [
   {
@@ -23,6 +24,13 @@ export const categories = [
 ];
 
 const Categories = () => {
+  const params = useSearchParams();
+  const category = params?.get("category");
+  const pathname = usePathname();
+
+  const isMainPage = pathname === "/";
+
+  if (!isMainPage) return null;
   return (
     <Container>
       <div className="flex flex-row items-center justify-between overflow-x-auto pt-4">
@@ -30,7 +38,7 @@ const Categories = () => {
           <CategoryBox
             key={item.label}
             label={item.label}
-            description={item.description}
+            selected={category === item.label}
             icon={item.icon}
           />
         ))}
