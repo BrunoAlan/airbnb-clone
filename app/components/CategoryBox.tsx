8b30095ab773a0a0bf1959d641bb47c6cc8bbbc4
Alan Bruno <alan.brunoab@gmail.com>
2023-04-18 10:54:34 -0300

    Update url on clicking category

--- app/components/CategoryBox.tsx
+++ app/components/CategoryBox.tsx
@@ -1,21 +1,51 @@
 "use client";
 
+import { useRouter, useSearchParams } from "next/navigation";
+import { useCallback } from "react";
 import { IconType } from "react-icons";
+import qs from "query-string";
 
 interface CategotyBoxProps {
   label: string;
   icon: IconType;
   selected?: boolean;
+  description?: string;
 }
 
 const CategoryBox = (props: CategotyBoxProps) => {
   const { label, icon: Icon, selected } = props;
+  const router = useRouter();
+  const params = useSearchParams();
+
+  const handleClick = useCallback(() => {
+    let currentQuery = {};
+    if (params) {
+      currentQuery = qs.parse(params.toString());
+    }
+    const updatedQuery: any = {
+      ...currentQuery,
+      category: label,
+    };
+    if (params?.get("category") === label) {
+      delete updatedQuery.category;
+    }
+
+    const url = qs.stringifyUrl(
+      {
+        url: "/",
+        query: updatedQuery,
+      },
+      { skipNull: true }
+    );
+    router.push(url);
+  }, [params, label, router]);
   return (
     <div
       className={`flex cursor-pointer flex-col items-center justify-center gap-3 border-b-2 p-3 transition hover:text-neutral-800 
       ${selected ? "border-b-neutral-800" : "border-transparent"}
       ${selected ? "text-neutral-800" : "text-neutral-500"}
       `}
+      onClick={handleClick}
     >
       <Icon size={26} />
       <div className="text-sm font-medium">{label}</div>
