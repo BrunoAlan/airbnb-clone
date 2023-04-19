41e7f9d68ca78c2bd250dc59d706b6e01f5e8bd7
Alan Bruno <alan.brunoab@gmail.com>
2023-04-18 22:25:33 -0300

    Rename getByValues hook as getByValue

--- app/hooks/useCountries.tsx
+++ app/hooks/useCountries.tsx
@@ -10,11 +10,11 @@ const formattedCountries = countries.map((country) => ({
 
 const useCountries = () => {
   const getAll = () => formattedCountries;
-  const getByValues = (value: string) => {
+  const getByValue = (value: string) => {
     return formattedCountries.find((item) => item.value === value);
   };
 
-  return { getAll, getByValues };
+  return { getAll, getByValue };
 };
 
 export default useCountries;
