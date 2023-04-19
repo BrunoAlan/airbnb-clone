650f7b1bab1fe65e9a53a8453abecaaa656a3539
BrunoAlan <alan.brunoab@gmail.com>
2023-04-19 12:12:01 -0300

    Update getListings to return SafeListing type

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
 import ListingCard from "./components/listings/ListingCard";
+import { SafeListing } from "./types";
 
 export default async function Home() {
   const listings = await getListings();
@@ -22,7 +23,7 @@ export default async function Home() {
       <ClientOnly>
         <Container>
           <div className="grid grid-cols-1 gap-8 pt-24 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
-            {listings.map((listing: any) => {
+            {listings.map((listing: SafeListing) => {
               return (
                 <ListingCard
                   key={listing.id}
