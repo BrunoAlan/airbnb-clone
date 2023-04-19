875f925ce97ad58e62545162ccbde0b9430dbf51
Alan Bruno <alan.brunoab@gmail.com>
2023-04-18 22:25:58 -0300

    Added ListingCard to main section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import getCurrentUser from "./actions/getCurrentUser";
+import getListings from "./actions/getListings";
 import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
+import ListingCard from "./components/listings/ListingCard";
 
-export default function Home() {
-  const isEmpty = true;
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+
+  const isEmpty = listings.length === 0;
 
   if (isEmpty)
     return (
@@ -16,7 +22,15 @@ export default function Home() {
       <ClientOnly>
         <Container>
           <div className="grid grid-cols-1 gap-8 pt-24 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
-            <div>Listings</div>
+            {listings.map((listing: any) => {
+              return (
+                <ListingCard
+                  key={listing.id}
+                  currentUser={currentUser}
+                  data={listing}
+                />
+              );
+            })}
           </div>
         </Container>
       </ClientOnly>
