a1de1dbc762e15f8899ae384c82081fdef812a1d
BrunoAlan <alan.brunoab@gmail.com>
2023-04-19 13:30:38 -0300

    Update getListingById.ts

--- app/actions/getListingById.ts
+++ app/actions/getListingById.ts
@@ -1,5 +1,4 @@
 import prisma from "@/app/libs/prismadb";
-import ListingCard from "../components/listings/ListingCard";
 
 interface IParams {
   listingId?: string;
@@ -8,6 +7,7 @@ interface IParams {
 export default async function getListingById(params: IParams) {
   try {
     const { listingId } = params;
+
     const listing = await prisma.listing.findUnique({
       where: {
         id: listingId,
@@ -16,16 +16,22 @@ export default async function getListingById(params: IParams) {
         user: true,
       },
     });
-    if (!listing) return null;
-    return { ...listing,
-       createdAt: listing.createdAt.toISOString(),
-      user:{
-            ...listing.user,
-            createdAt:listing.user.createdAt.toISOString(),
-            updatedAt:listing.user.updatedAt.toISOString(),
-            emailVerified:listing.user.emailVerified?.toISOString() || null
-      }};
-  } catch (error:any) {
-      throw new Error(error)
+
+    if (!listing) {
+      return null;
+    }
+
+    return {
+      ...listing,
+      createdAt: listing.createdAt.toString(),
+      user: {
+        ...listing.user,
+        createdAt: listing.user.createdAt.toString(),
+        updatedAt: listing.user.updatedAt.toString(),
+        emailVerified: listing.user.emailVerified?.toString() || null,
+      },
+    };
+  } catch (error: any) {
+    throw new Error(error);
   }
 }
