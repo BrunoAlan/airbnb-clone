1cb38534c158d3582240297b5635f870764e5bc1
BrunoAlan <alan.brunoab@gmail.com>
2023-04-19 20:38:02 -0300

    TripsClientProps set reservations as SafeReservation array

--- app/trips/TripsClient.tsx
+++ app/trips/TripsClient.tsx
@@ -9,7 +9,7 @@ import { toast } from "react-hot-toast";
 import ListingCard from "../components/listings/ListingCard";
 
 interface TripsClientProps {
-  reservations: SafeReservation;
+  reservations: SafeReservation[];
   currentUser: SafeUser | null;
 }
 
@@ -41,7 +41,7 @@ const TripsClient = (props: TripsClientProps) => {
         subtitle="Where you have been and where you're going"
       />
       <div className="mt-10 grid grid-cols-2 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
-        {reservations.map((reservation) => (
+        {reservations.map((reservation: any) => (
           <ListingCard
             key={reservation.id}
             data={reservation.listing}
