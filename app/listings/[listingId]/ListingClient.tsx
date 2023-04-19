687c457b5c399a751cf62db0cbb121e667f5527c
BrunoAlan <alan.brunoab@gmail.com>
2023-04-19 15:11:27 -0300

    Invert order of parameters in differenceInCalendarDays

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -75,8 +75,8 @@ const ListingClient = ({
   useEffect(() => {
     if (dateRange.startDate && dateRange.endDate) {
       const dayCount = differenceInCalendarDays(
-        dateRange.startDate,
-        dateRange.endDate
+        dateRange.endDate,
+        dateRange.startDate
       );
       if (dayCount && listing.price) {
         setTotalPrice(dayCount * listing.price);
