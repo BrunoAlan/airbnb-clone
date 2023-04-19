f87eed10437783221f632faa41ecc2f7c1111608
BrunoAlan <alan.brunoab@gmail.com>
2023-04-19 14:24:32 -0300

    Create ListingReservationsProps interface

--- app/components/listings/ListingReservations.tsx
+++ app/components/listings/ListingReservations.tsx
@@ -1,5 +1,14 @@
 "use client";
-import React from "react";
+import { Range } from "react-date-range";
+interface ListingReservationsProps {
+  price: number;
+  dateRange: Range;
+  totalPrice: number;
+  onChangeDate: (value: Range) => void;
+  onSubmit: () => void;
+  disabled?: boolean;
+  disabledDates: Date[];
+}
 
 const ListingReservations = () => {
   return <div>ListingReservations</div>;
