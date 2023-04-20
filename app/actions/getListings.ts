e466cbffddf5d7472670bafdad92b51e59713371
BrunoAlan <alan.brunoab@gmail.com>
2023-04-19 21:15:18 -0300

    Added query for users to GetListings

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -6,7 +6,16 @@ export interface IListingParams {
 
 export default async function getListings(params: IListingParams) {
   try {
+    const { userId } = params
+
+    let query: any = {}
+
+    if (userId) {
+      query.userId = userId
+    }
+
     const listings = await prisma.listing.findMany({
+      where: query,
       orderBy: {
         createdAt: "desc",
       },
