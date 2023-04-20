8cddefaf957c5e06e7aa54a89bbb75b005b13f16
BrunoAlan <alan.brunoab@gmail.com>
2023-04-19 21:35:02 -0300

    Create PropertiesClient.tsx

---
+++ app/properties/PropertiesClient.tsx
@@ -0,0 +1,57 @@
+"use client";
+import { useRouter } from "next/navigation";
+import Container from "../components/Container";
+import Heading from "../components/Heading";
+import { SafeListing, SafeReservation, SafeUser } from "../types";
+import { useCallback, useState } from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import ListingCard from "../components/listings/ListingCard";
+
+interface PropertiesClientProps {
+  listings: SafeListing[];
+  currentUser: SafeUser | null;
+}
+
+const PropertiesClient = (props: PropertiesClientProps) => {
+  const { listings, currentUser } = props;
+
+  const router = useRouter();
+  const [deletingIg, setDeletingId] = useState("");
+
+  const onCancel = useCallback(
+    (id: string) => {
+      setDeletingId(id);
+      axios
+        .delete(`/api/listings/${id}`)
+        .then(() => {
+          toast.success("Listing deleted");
+          router.refresh();
+        })
+        .catch((error) => toast.error(error?.response?.data?.error))
+        .finally(() => setDeletingId(""));
+    },
+    [router]
+  );
+
+  return (
+    <Container>
+      <Heading title="Properties" subtitle="List of your properties" />
+      <div className="mt-10 grid grid-cols-2 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+        {listings.map((listing: any) => (
+          <ListingCard
+            key={listing.id}
+            data={listing}
+            actionId={listing.id}
+            onAction={onCancel}
+            disabled={deletingIg === listing.id}
+            actionLabel="Cancel reservation"
+            currentUser={currentUser}
+          />
+        ))}
+      </div>
+    </Container>
+  );
+};
+
+export default PropertiesClient;
