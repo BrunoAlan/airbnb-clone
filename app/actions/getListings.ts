2aee3a74948a5c1a4d4bafe85532ecb2668bae4b
BrunoAlan <alan.brunoab@gmail.com>
2023-04-19 22:00:46 -0300

    Update getListings params to extend search functionality

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -1,10 +1,17 @@
 import prisma from "@/app/libs/prismadb";
 
-export interface IListingParams {
-  userId?: string
+export interface IListingsParams {
+  userId?: string;
+  guestCount?: number;
+  roomCount?: number;
+  bathroomCount?: number;
+  startDate?: string;
+  endDate?: string;
+  locationValue?: string;
+  category?: string;
 }
 
-export default async function getListings(params: IListingParams) {
+export default async function getListings(params: IListingsParams) {
   try {
     const { userId } = params
 
